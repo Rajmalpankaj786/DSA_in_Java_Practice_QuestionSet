37abcec1ee3cebb567cf2b8db06ff9fa679f643a
Rajmalpankaj786 <rajmalpankaj786@gmail.com>
2023-11-14 00:03:01 +0530

    missing element in array

---
+++ DSA_problem_in_java/src/com/DSA/ArrayProblems/MissingElement_In_an_Array.java
@@ -0,0 +1,19 @@
+package com.DSA.ArrayProblems;
+
+public class MissingElement_In_an_Array {
+     
+	public static int missingElement(int[] arr ) {
+		int n = arr.length+1;
+	    int sum = n*(n+1)/2;
+	    for(int num : arr) {
+	    	sum = sum-num;
+	    }
+	    return sum;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = new int[] {1,2,0,5,6};
+		System.out.println(missingElement(arr));
+		
+	}
+}
