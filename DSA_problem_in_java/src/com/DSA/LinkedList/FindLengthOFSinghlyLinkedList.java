fde402c406175536494e16341ca9468985b10ba6
Rajmalpankaj786 <rajmalpankaj786@gmail.com>
2023-11-15 12:04:46 +0530

    find length of singly linked list

--- DSA_problem_in_java/src/com/DSA/LinkedList/FindLengthOFSinghlyLinkedList.java
+++ DSA_problem_in_java/src/com/DSA/LinkedList/FindLengthOFSinghlyLinkedList.java
@@ -1,5 +1,47 @@
 package com.DSA.LinkedList;
 
 public class FindLengthOFSinghlyLinkedList {
-
+      private ListNode head;
+      
+      private static class ListNode{
+    	  private int data;
+    	  private ListNode next ;
+		public ListNode(int data) {
+			
+			this.data = data;
+			this.next = null;
+		}
+    	  
+      }
+      
+      public int printLength() {
+    	  int length = 0;
+    	  ListNode current = head;
+    	  while(current != null) {
+    		  //System.out.println(current.data +"->");
+    		  current = current.next;
+    		  length++;
+    	  }
+    	   
+    	  return length;
+    	  
+      }
+      
+      public static void main(String[] args) {
+    	  FindLengthOFSinghlyLinkedList sll = new FindLengthOFSinghlyLinkedList();
+    	  
+    	  sll.head = new ListNode(10);
+    	  ListNode second = new ListNode(11);
+    	  ListNode third = new ListNode(12);
+    	  ListNode forth = new ListNode(13);
+    	  
+    	  // set connection to the nodes
+    	  
+    	  sll.head.next = second;
+    	  second.next = third;
+    	  third.next = forth;
+    	  
+    	System.out.println(  sll.printLength());
+    	  
+	}
 }
